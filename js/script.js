b8d88836a49a2c90f5ec8a3f22b9e8fed3b69751
margo <megi.busenka@gmail.com>
2025-11-11 00:52:12 +0200

    The cleaning error has been fixed, a button to delete the second number has been added, and the calculation operations have been completed.

--- js/script.js
+++ js/script.js
@@ -4,11 +4,12 @@ let sing = "";
 let finish = false;
 
 const operator = ["+", "-", "*", "/", "%"];
-const digit = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
+const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
 
 const out = document.querySelector(".calc-screen p");
 const clear = document.querySelector(".clear");
 const buttons = document.querySelector(".buttons");
+const btnDelete = document.querySelector(".del");
 
 function clearAll() {
   firstNum = "";
@@ -16,32 +17,77 @@ function clearAll() {
   sing = "";
   finish = false;
   out.textContent = "0";
+  console.log("clearAll");
 }
-clear.addEventListener("click", clearAll());
+clear.addEventListener("click", clearAll);
+
+function deleteSecondNum() {
+  secondNum = "";
+  out.textContent = "";
+  console.log("delete two number");
+}
+
+btnDelete.addEventListener("click", deleteSecondNum);
 
 buttons.addEventListener("click", (event) => {
   if (!event.target.classList.contains("btn")) return;
-  out.textContent = "";
   //первое число
   const key = event.target.textContent;
   if (digit.includes(key)) {
     if (secondNum === "" && sing === "") {
       firstNum += key;
       out.textContent = firstNum;
-      console.log(firstNum, secondNum, sing);
+      console.log("первое число", firstNum);
     } else if (firstNum !== "" && secondNum !== "" && finish) {
-      
+      secondNum = key;
+      finish = false;
+      out.textContent = secondNum;
     } else {
       secondNum += key;
       out.textContent = secondNum;
-      console.log(secondNum);
+      console.log("второе число", secondNum);
     }
+    return;
   }
   //знак +-/ знаки
   if (operator.includes(key)) {
-    sing += key;
+    sing = key;
     out.textContent = sing;
     console.log(sing);
     return;
+  }
+  //click equal
+  if (key === "=") {
+    if (secondNum === "") secondNum = firstNum;
+    switch (sing) {
+      case "+":
+        firstNum = +firstNum + +secondNum;
+        break;
+      case "-":
+        firstNum = +firstNum - +secondNum;
+        break;
+      case "*":
+        firstNum = +firstNum * +secondNum;
+        break;
+      case "/":
+        if (+secondNum === 0) {
+          out.textContent = "error";
+          firstNum = "";
+          secondNum = "";
+          sing = "";
+          finish = true;
+          return;
+        }
+        firstNum = +firstNum / +secondNum;
+        break;
+      case "%":
+        firstNum = (+firstNum * +secondNum) / 100;
+        break;
+      default:
+        out.textContent = "error";
+    }
+    finish = true;
+    out.textContent = firstNum;
+    console.log(firstNum);
   }
 });
