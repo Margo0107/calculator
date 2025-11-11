cfe681058d061ac974c73af65743461c92ec3f6e
Margo0107 <megi.busenka@gmail.com>
2025-11-12 00:34:12 +0200

    add button theme

---
+++ js/light-thrme.js
@@ -0,0 +1,6 @@
+const btnTheme = document.querySelector(".icon-theme");
+
+btnTheme.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  //   console.log(event.target);
+});
